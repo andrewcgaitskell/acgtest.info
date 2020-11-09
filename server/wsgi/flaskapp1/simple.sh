50d30293f17748dd3a8dcdae4a81e4bd022455f1
andrewcgaitskell <andrew@gaitskell.com>
2020-11-09 18:49:05 +0000

    Update simple.sh

--- server/wsgi/flaskapp1/simple.sh
+++ server/wsgi/flaskapp1/simple.sh
@@ -1,1 +1,1 @@
-uwsgi --socket 0.0.0.0:5000 --wsgi-file project1.py --callable app --processes 4 --threads 2 --buffer_size 65535
+uwsgi --socket 0.0.0.0:5000 --wsgi-file project1.py --callable app --processes 4 --threads 2 -b 32768
