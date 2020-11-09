7f325e1b6a7b09363c6f1253cb679520fd241115
andrewcgaitskell <andrew@gaitskell.com>
2020-11-09 14:14:29 +0000

    Update startservice1.sh

--- server/wsgi/flaskapp1/startservice1.sh
+++ server/wsgi/flaskapp1/startservice1.sh
@@ -1,7 +1,9 @@
-sudo cp project1.service /etc/systemd/system/project1.service
+sudo cp /var/www/acgtest.info/server/wsgi/flaskapp1/project1.service.txt /etc/systemd/system/project1.service
 
 sudo systemctl start project1
 sudo systemctl enable project1
+
+sudo cp /var/www/acgtest.info/server/wsgi/flaskapp1/project1.sites-available.txt /etc/nginx/sites-available/project1
 
 sudo ln -s /etc/nginx/sites-available/project1 /etc/nginx/sites-enabled
 
