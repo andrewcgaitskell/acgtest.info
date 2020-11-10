d4d7191ef71caf9acf09abb1397c122bd4db9560
andrewcgaitskell <andrew@gaitskell.com>
2020-11-10 17:11:30 +0000

    Update 001_1_createenv_mqttclient1.sh

--- server/gunicorn/mqttclient1/001_1_createenv_mqttclient1.sh
+++ server/gunicorn/mqttclient1/001_1_createenv_mqttclient1.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
+PROJECT_NAME = "mqttclient1"
+FRAMEWORK = "gunicorn"
 
-sudo rm -rf /var/www/acgtest.info/server/gunicorn/mqttclient1/env
+sudo rm -rf /var/www/acgtest.info/server/$FRAMEWORK/$PROJECT_NAME/env
 
 python3 -m venv env
