f79fd3f44f5851c1f7ba74752d051e41f81c9b0b
andrewcgaitskell <andrew@gaitskell.com>
2020-11-10 09:37:51 +0000

    Update wsgi.py

--- server/gunicorn/dashboards1/wsgi.py
+++ server/gunicorn/dashboards1/wsgi.py
@@ -1,8 +1,8 @@
 from app import app
 
-PORT = 5000;
-#ADDRESS = '127.0.0.1'
-ADDRESS = '0.0.0.0'
+PORT = 5004;
+ADDRESS = '127.0.0.1'
+#ADDRESS = '0.0.0.0'
 
 if __name__ == '__main__':
     app.run_server(
