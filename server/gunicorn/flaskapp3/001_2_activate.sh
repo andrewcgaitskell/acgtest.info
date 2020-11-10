70248dbc74986556b5428ce387fd85367df6785b
andrewcgaitskell <andrew@gaitskell.com>
2020-11-10 06:41:25 +0000

    created project3

---
+++ server/gunicorn/flaskapp3/001_2_activate.sh
@@ -0,0 +1,1 @@
+source /var/www/acgtest.info/server/wsgi/flaskapp2/env/bin/activate
---
+++ server/gunicorn/flaskapp3/002_startservice3.sh
@@ -0,0 +1,19 @@
+sudo systemctl stop project2
+sudo systemctl disable project2
+
+sudo cp /var/www/acgtest.info/server/wsgi/flaskapp2/project2.service.txt /etc/systemd/system/project2.service
+
+sudo systemctl daemon-reload
+
+sudo systemctl start project2
+sudo systemctl enable project2
+
+sudo systemctl status project2
+
+sudo cp /var/www/acgtest.info/server/build/acgtest.info /etc/nginx/sites-available/acgtest.info
+
+sudo nginx -t
+
+sudo systemctl restart nginx
+
+sudo systemctl status nginx
