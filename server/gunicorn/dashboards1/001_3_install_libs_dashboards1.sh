6255955b7c4c750eecf70f3adf684615fba16bd5
andrewcgaitskell <andrew@gaitskell.com>
2020-11-10 09:12:52 +0000

    Update 001_3_install_libs_dashboards1.sh

--- server/gunicorn/dashboards1/001_3_install_libs_dashboards1.sh
+++ server/gunicorn/dashboards1/001_3_install_libs_dashboards1.sh
@@ -3,3 +3,7 @@ pip install uwsgi
 pip install Flask
 pip install gunicorn
 pip install dash
+pip install dash_core_components
+pip install dash_html_components as html
+pip install plotly
+pip install pandas
