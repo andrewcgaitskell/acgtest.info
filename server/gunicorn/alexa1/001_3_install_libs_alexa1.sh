3c053589416d883f17042afd97c2154f250754b8
andrewcgaitskell <andrew@gaitskell.com>
2020-11-10 12:33:48 +0000

    Update 001_3_install_libs_alexa1.sh

--- server/gunicorn/alexa1/001_3_install_libs_alexa1.sh
+++ server/gunicorn/alexa1/001_3_install_libs_alexa1.sh
@@ -2,8 +2,5 @@ pip install wheel
 pip install uwsgi
 pip install Flask
 pip install gunicorn
-pip install dash
-pip install dash_core_components
-pip install dash_html_components as html
-pip install plotly
 pip install pandas
+pip install ask-sdk
