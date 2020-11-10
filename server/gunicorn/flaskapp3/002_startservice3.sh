f51517b82bbfc2fe7dd72dd9d82c65a6e4fda1f5
andrewcgaitskell <andrew@gaitskell.com>
2020-11-10 06:44:45 +0000

    Update 002_startservice3.sh

--- server/gunicorn/flaskapp3/002_startservice3.sh
+++ server/gunicorn/flaskapp3/002_startservice3.sh
@@ -1,14 +1,14 @@
-sudo systemctl stop project2
-sudo systemctl disable project2
+sudo systemctl stop project3
+sudo systemctl disable project3
 
-sudo cp /var/www/acgtest.info/server/wsgi/flaskapp2/project2.service.txt /etc/systemd/system/project2.service
+sudo cp /var/www/acgtest.info/server/gunicorn/flaskapp3/project3.service.txt /etc/systemd/system/project3.service
 
 sudo systemctl daemon-reload
 
-sudo systemctl start project2
-sudo systemctl enable project2
+sudo systemctl start project3
+sudo systemctl enable project3
 
-sudo systemctl status project2
+sudo systemctl status project3
 
 sudo cp /var/www/acgtest.info/server/build/acgtest.info /etc/nginx/sites-available/acgtest.info
 
