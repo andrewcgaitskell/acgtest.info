9b75ade27fe8bc8046a73bca8812178d33f42002
andrewcgaitskell <andrew@gaitskell.com>
2020-11-11 11:45:02 +0000

    Update base.py

--- server/gunicorn/mqttclient1/base.py
+++ server/gunicorn/mqttclient1/base.py
@@ -4,6 +4,12 @@ import paho.mqtt.publish as publish
 import paho.mqtt.client as mqtt
 import ssl
 
+import configparser
+config = configparser.ConfigParser()
+config.sections()
+config.read('app.ini')
+
+
 auth = {
   'username':"ciscohackhub.azure-devices.net/lora1",
   'password':"SharedAccessSignature sr=ciscohackhub.azure-devices.net%2Fdevices%2Flora1&sig=xxxx&se=1463048772"
