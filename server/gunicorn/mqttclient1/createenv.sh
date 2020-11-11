550305bd6c5a4fa10dc140c1dce141fbc9485e3d
andrewcgaitskell <andrew@gaitskell.com>
2020-11-11 09:58:53 +0000

    Update createenv.sh

--- server/gunicorn/mqttclient1/createenv.sh
+++ server/gunicorn/mqttclient1/createenv.sh
@@ -7,5 +7,15 @@ sudo rm -rf /var/www/acgtest.info/server/$FRAMEWORK/$PROJECT_NAME/env
 python3 -m venv env
 set -e
 source $PWD/env/bin/activate
+
 pip install pandas
+pip install wheel
+pip install uwsgi
+pip install Flask
+pip install gunicorn
+pip install pandas
+pip install flask_mqtt
+pip install flask_socketio
+pip install flask_bootstrap
+
 deactivate
