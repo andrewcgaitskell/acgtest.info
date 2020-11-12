524455786df0a9fe6a1c090a418f5d55961970bb
andrewcgaitskell <andrew@gaitskell.com>
2020-11-12 10:00:06 +0000

    Update simple.py

--- server/gunicorn/api1/simple.py
+++ server/gunicorn/api1/simple.py
@@ -1,8 +1,8 @@
 from app import app
 
-PORT = 5005;
+PORT = 5000;
 #ADDRESS = '127.0.0.1'
 ADDRESS = '0.0.0.0'
 
 if __name__ == '__main__':
-    socketio.run(app,port=5005, use_reloader=False, debug=False)
+    socketio.run(app,port=5000, use_reloader=False, debug=False)
