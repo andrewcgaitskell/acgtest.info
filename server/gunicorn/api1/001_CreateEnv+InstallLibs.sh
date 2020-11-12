9fea6ea6b8c29b12535af1063f4403a01f9db613
andrewcgaitskell <andrew@gaitskell.com>
2020-11-12 09:42:55 +0000

    Update 001_CreateEnv+InstallLibs.sh

--- server/gunicorn/api1/001_CreateEnv+InstallLibs.sh
+++ server/gunicorn/api1/001_CreateEnv+InstallLibs.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-PROJECT_NAME='mqttclient1'
+PROJECT_NAME='api1'
 FRAMEWORK='gunicorn'
 
 sudo rm -rf /var/www/acgtest.info/server/$FRAMEWORK/$PROJECT_NAME/env
@@ -8,24 +8,15 @@ python3 -m venv env
 set -e
 source $PWD/env/bin/activate
 
-pip install dash
-pip install dash_core_components
-pip install dash_html_components
-pip install plotly
-
 pip install eventlet
 
 pip install configparser
 
 pip install pandas
 pip install wheel
-pip install uwsgi
 pip install Flask
 pip install gunicorn
-pip install pandas
 pip install flask_mqtt
-pip install flask_socketio
-pip install flask_bootstrap
 pip install eventlet
 
 deactivate
