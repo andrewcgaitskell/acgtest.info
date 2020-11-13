1ca05d5400b01989bccecc97b497d0c1729390d6
andrewcgaitskell <andrew@gaitskell.com>
2020-11-13 11:18:33 +0000

    Update app.py

--- server/gunicorn/api1/app.py
+++ server/gunicorn/api1/app.py
@@ -92,4 +92,5 @@ def login():
         return 'show the form'
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    #app.run(debug=True)
+    socketio.run(app, host='0.0.0.0', port=5000, use_reloader=False, debug=False)
