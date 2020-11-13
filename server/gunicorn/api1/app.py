724b8f91fe323b67476009506847b03d2a1a4b4a
andrewcgaitskell <andrew@gaitskell.com>
2020-11-13 07:41:12 +0000

    Update app.py

--- server/gunicorn/api1/app.py
+++ server/gunicorn/api1/app.py
@@ -1,7 +1,8 @@
 from flask import Flask
 from flask_restful import Resource, Api
+from markupsafe import escape
 
-app = Flask('api1')
+app = Flask(__name__)
 api = Api(app)
 
 class HelloWorld(Resource):
@@ -9,6 +10,22 @@ class HelloWorld(Resource):
         return {'hello': 'world'}
 
 api.add_resource(HelloWorld, '/')
+
+
+@app.route('/user/<username>')
+def show_user_profile(username):
+    # show the user profile for that user
+    return 'User %s' % escape(username)
+
+@app.route('/post/<int:post_id>')
+def show_post(post_id):
+    # show the post with the given id, the id is an integer
+    return 'Post %d' % post_id
+
+@app.route('/path/<path:subpath>')
+def show_subpath(subpath):
+    # show the subpath after /path/
+    return 'Subpath %s' % escape(subpath)
 
 if __name__ == '__main__':
     app.run(debug=True)
