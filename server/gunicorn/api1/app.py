31bef7c70b6a8f6d1c59112e495402ebcb74106a
andrewcgaitskell <andrew@gaitskell.com>
2020-11-13 12:49:38 +0000

    Update app.py

--- server/gunicorn/api1/app.py
+++ server/gunicorn/api1/app.py
@@ -93,4 +93,4 @@ def login():
 
 if __name__ == '__main__':
     #app.run(debug=True)
-    socketio.run(app, host='0.0.0.0', port=5000, use_reloader=False, debug=False)
+    socketio.run(app)#, host='0.0.0.0', port=5000, use_reloader=False, debug=False)
