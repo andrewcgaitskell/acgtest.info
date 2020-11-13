cc6c10d6522082391b6c3e1791e4d10d744102e0
andrewcgaitskell <andrew@gaitskell.com>
2020-11-13 19:19:40 +0000

    Update 001_CreateEnv+InstallLibs.sh

--- server/gunicorn/alexa1/001_CreateEnv+InstallLibs.sh
+++ server/gunicorn/alexa1/001_CreateEnv+InstallLibs.sh
@@ -8,23 +8,6 @@ python3 -m venv env
 set -e
 source $PWD/env/bin/activate
 
-pip install eventlet
-
-pip install configparser
-
-pip install pandas
-pip install wheel
-pip install Flask
-pip install flask-ask
-pip install flask_restful
-pip install gunicorn
-pip install flask_mqtt
-pip install flask_socketio
-pip install flask_bootstrap
-
-#pip install ask-sdk
-#pip install ask-sdk-webservice-support
-
-pip install eventlet
+pip install -r requirements.txt
 
 deactivate
