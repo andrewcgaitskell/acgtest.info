f6024adb1b04872fa35d7905c42ec16e497c1fb3
andrewcgaitskell <andrew@gaitskell.com>
2020-11-15 08:17:09 +0000

    Update simple_voice1.sh

--- server/gunicorn/voice1/simple_voice1.sh
+++ server/gunicorn/voice1/simple_voice1.sh
@@ -1,2 +1,3 @@
-gunicorn -b 127.0.0.1:5000 --worker-class eventlet -w 1 wsgi:app
+#gunicorn -b 127.0.0.1:5000 --worker-class eventlet -w 1 wsgi:app
 #gunicorn --worker-class eventlet -w 1 wsgi:app
+gunicorn -b 0.0.0.0:5000 --worker-class eventlet -w 1 wsgi:app
