8334fe941abac99b133d3eb9fdb50b5a4b5f981e
andrewcgaitskell <andrew@gaitskell.com>
2020-11-15 08:11:27 +0000

    Update wsgi.py

--- server/gunicorn/voice1/wsgi.py
+++ server/gunicorn/voice1/wsgi.py
@@ -1,9 +1,9 @@
 from app import app
 
-PORT = 5006;
+PORT = 5008;
 ADDRESS = '127.0.0.1'
 #ADDRESS = '0.0.0.0'
 
 if __name__ == '__main__':
-    socketio.run(app,use_reloader=False, debug=False)
+    socketio.run(app,port=PORT, use_reloader=False, debug=False)
 
