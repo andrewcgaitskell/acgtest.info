bae16f11751a21419d249f68760d572eac0d9b4c
andrewcgaitskell <andrew@gaitskell.com>
2020-11-23 14:08:56 +0000

    removed voice

--- server/gunicorn/voice1/003_checkservice_voice1.sh
+++
@@ -1,3 +0,0 @@
-PROJECT_NAME="voice1"
-FRAMEWORK="gunicorn"
-sudo systemctl status $PROJECT_NAME
--- server/gunicorn/voice1/simple_voice1.sh
+++
@@ -1,3 +0,0 @@
-#gunicorn -b 127.0.0.1:5000 --worker-class eventlet -w 1 wsgi:app
-#gunicorn --worker-class eventlet -w 1 wsgi:app
-gunicorn -b 0.0.0.0:5000 --worker-class eventlet -w 1 wsgi:app
